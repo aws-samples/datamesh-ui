9c107444aee20db238a11432bc95ef3fa4ec71df
Jan Tan <jantan@amazon.com>
2022-09-14 11:39:38 +0700

    Added backend validation of s3 paths

--- src/Components/RegisterNewProductComponent.js
+++ src/Components/RegisterNewProductComponent.js
@@ -16,8 +16,9 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 import { GetDatabaseCommand, GlueClient } from "@aws-sdk/client-glue";
+import { ListObjectsV2Command, S3Client } from "@aws-sdk/client-s3";
 import { SFNClient, StartExecutionCommand } from "@aws-sdk/client-sfn";
-import { Box, Button, Container, Form, FormField, Header, Input, Select, SpaceBetween, Table, Icon, Alert, ColumnLayout, BreadcrumbGroup } from "@cloudscape-design/components";
+import { Box, Button, Container, Form, FormField, Header, Input, Select, SpaceBetween, Table, Icon, Alert, ColumnLayout, BreadcrumbGroup, Spinner } from "@cloudscape-design/components";
 import {Amplify, Auth } from "aws-amplify";
 import {useEffect, useState} from 'react';
 import { useParams } from "react-router";
@@ -25,6 +26,7 @@ import {v4 as uuid} from 'uuid';
 import ResourceLFTagsComponent from "./TBAC/ResourceLFTagsComponent";
 import ValueWithLabel from "./ValueWithLabel";
 const cfnOutput = require("../cfn-output.json");
+const axios = require("axios").default;
 
 const config = Amplify.configure();
 const dpmStateMachineArn = cfnOutput.InfraStack.DPMStateMachineArn;
@@ -33,14 +35,15 @@ function RegisterNewProductComponent() {
     const {domainId} = useParams()
     const [error, setError] = useState();
     const [database, setDatabase] = useState(null)
-    const [products, setProducts] = useState([{"id": uuid(), "name": "", "location": "", "firstRow": true}])
+    const [products, setProducts] = useState([{"id": uuid(), "name": "", "location": "", "error": "", "nameError": "", "firstRow": true}])
+    const [spinnerVisible, setSpinnerVisible] = useState(false)
  
     // const onCancel = () => {
     //     window.location.href="/product-registration/list";
     // }
 
     const addProductRow = () => {
-        setProducts([...products, {"id": uuid(), "name": "", "location": "", "firstRow": false}])
+        setProducts([...products, {"id": uuid(), "name": "", "location": "", "error": "", "nameError": "", "firstRow": false}])
     }
 
     const removeProductRow = (id) => {
@@ -55,29 +58,120 @@ function RegisterNewProductComponent() {
 
     const onSubmit = async() => {
         if (database && isProductListValid()) {
+            setSpinnerVisible(true)
             const credentials = await Auth.currentCredentials();
-            const sfnClient = new SFNClient({region: config.aws_project_region, credentials: Auth.essentialCredentials(credentials)});
-            const accessMode = (database.Database.Parameters && database.Database.Parameters.access_mode) ? database.Database.Parameters.access_mode : "nrac";
-            const formattedProducts = products.map((prod) => {
-                prod.location = `${database.Database.LocationUri}/${prod.location}`
-                prod.location_key = prod.location.substring(5);
-                
-                return prod;
-            })
-            await sfnClient.send(new StartExecutionCommand({
-                stateMachineArn: dpmStateMachineArn,
-                input: JSON.stringify({
-                    "producer_acc_id": database.Database.Parameters.data_owner,
-                    "database_name": domainId,
-                    "lf_access_mode": accessMode,
-                    "tables": formattedProducts
-                })
-            }))
+            const isPathValid = await isS3PathsValid(credentials)
 
-            window.location.href = `/tables/${domainId}`
+            if (isPathValid) {
+                const sfnClient = new SFNClient({region: config.aws_project_region, credentials: Auth.essentialCredentials(credentials)});
+                const accessMode = (database.Database.Parameters && database.Database.Parameters.access_mode) ? database.Database.Parameters.access_mode : "nrac";
+                const formattedProducts = products.map((prod) => {
+                    if (prod.location.startsWith("/")) {
+                        prod.location = `${database.Database.LocationUri}${prod.location}`
+                    } else {
+                        prod.location = `${database.Database.LocationUri}/${prod.location}`
+                    }
+                    
+                    prod.location_key = prod.location.substring(5);
+                    
+                    return prod;
+                })
+                await sfnClient.send(new StartExecutionCommand({
+                    stateMachineArn: dpmStateMachineArn,
+                    input: JSON.stringify({
+                        "producer_acc_id": database.Database.Parameters.data_owner,
+                        "database_name": domainId,
+                        "lf_access_mode": accessMode,
+                        "tables": formattedProducts
+                    })
+                }))
+    
+                window.location.href = `/tables/${domainId}`
+            } else {
+                setSpinnerVisible(false)
+            }
         } else {
             setError("Missing required fields.");
         }
+    }
+
+    const isS3PathsValid = async() => {
+        let isValid = true
+        const tempProducts = []
+        const pathTokenized = database.Database.LocationUri.split("/")
+        const bucket = pathTokenized[2]
+        const locationTopLevelPrefix = pathTokenized[3]
+        const validateProductsPayload = {
+            "bucket": bucket,
+            "products": []
+        }
+        for (const prod of products) {
+            const nameLength = prod.name.length
+
+            if (nameLength < 1 || nameLength > 255) {
+                isValid = false
+                prod.nameError = "Invalid name. Must be between 1 and 255 characters long."
+            }
+
+            if (!prod.location.endsWith("/")) {
+                isValid = false;
+                prod.error = "Invalid path. Path should reference a folder not an object."
+            } else {
+                let prodPrefix = null;
+
+                if (prod.location.startsWith("/")) {
+                    prodPrefix = locationTopLevelPrefix+prod.location
+                } else {
+                    prodPrefix = locationTopLevelPrefix+"/"+prod.location
+                }
+                
+
+                validateProductsPayload.products.push({
+                    prefix: prodPrefix,
+                    id: prod.id
+                })
+            }
+
+            tempProducts.push(prod)
+        }
+
+        if (!isValid) {
+            setProducts(tempProducts)
+        } else {
+            const validateUrl  =`${cfnOutput.InfraStack.WorkflowApiUrl}/data-products/validate`
+            const session = await Auth.currentSession()
+            
+            const response = await axios({
+                method: "POST",
+                url: validateUrl,
+                headers: {
+                    "Authorization": session.getAccessToken().getJwtToken(),
+                    "Content-Type": "application/json"
+                },
+                data: validateProductsPayload
+            })
+
+            const validateData = response.data
+
+            isValid = validateData.valid
+
+            if (!isValid) {
+                const prodMap = validateData.products
+                const validatedProducts = products.map((row) => {
+                    const validationDetails = prodMap[row.id]
+
+                    if (validationDetails.error) {
+                        row.error = validationDetails.error
+                    }
+
+                    return row
+                })
+
+                setProducts(validatedProducts)
+            }
+        }
+
+        return isValid;
     }
 
     const isProductListValid = () => {
@@ -133,6 +227,18 @@ function RegisterNewProductComponent() {
         })()
     }, [])
 
+    const renderSubmit = () => {
+        if (spinnerVisible) {
+            return (
+                <Button disabled="true"><Spinner /> Submit</Button>
+            )
+        } else {
+            return (
+                <Button variant="primary" onClick={onSubmit} disabled={!database}>Submit</Button>
+            )
+        }
+    }
+
     return (
         <Box>
             <BreadcrumbGroup items={[
@@ -143,8 +249,8 @@ function RegisterNewProductComponent() {
             <Box margin={{top: "m"}}>
                 <Form errorText={error} actions={
                     <SpaceBetween direction="horizontal" size="s">
-                        <Button variant="link" href={`/tables/${domainId}`}>Cancel</Button>
-                        <Button variant="primary" onClick={onSubmit} disabled={!database}>Submit</Button>
+                        <Button variant="link" href={`/tables/${domainId}`} disabled={spinnerVisible}>Cancel</Button>
+                        {renderSubmit()}
                     </SpaceBetween>
                 }>
                     <Box margin={{top: "m"}}>
@@ -156,11 +262,11 @@ function RegisterNewProductComponent() {
                         <Table footer={<Button onClick={() => addProductRow()}><Icon name="add-plus" /> Add</Button>} header={<Header variant="h3">Products</Header>} columnDefinitions={[
                             {
                                 header: "Name",
-                                cell: e => <Input type="text" placeholder="Enter Name" value={e.name} onChange={(event) => updateField(e.id, "name", event.detail.value)} />
+                                cell: e => <FormField errorText={e.nameError} constraintText="Not less than 1 or more than 255 bytes long."><Input type="text" placeholder="Enter Name" value={e.name} onChange={(event) => updateField(e.id, "name", event.detail.value)} /></FormField>
                             },
                             {
                                 header: "S3 Location (Prefix)",
-                                cell: e => <Input type="text" placeholder="Input prefix relative to Data Domain Location URI" value={e.location}  onChange={(event) => updateField(e.id, "location", event.detail.value)} />
+                                cell: e => <FormField constraintText={(database) ? `Input folder path relative to ${database.Database.LocationUri}. Path must end with /` : ""} errorText={e.error}><Input type="text" value={e.location}  onChange={(event) => updateField(e.id, "location", event.detail.value)} /></FormField>
                             },
                             {
                                 header: "",
