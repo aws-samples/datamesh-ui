62aac433bb629dce1f4861852f04bdc57346bb3a
eistrati <eistrati@users.noreply.github.com>
2023-01-19 13:57:11 -0500

    minor improvements

--- instructor/create_aws_profiles.sh
+++ instructor/create_aws_profiles.sh
@@ -9,14 +9,12 @@ aws configure set profile.central.aws_secret_access_key $AWS_SECRET_ACCESS_KEY_C
 aws configure set profile.central.aws_session_token $AWS_SESSION_TOKEN_CENTRAL
 aws configure set profile.central.region $AWS_REGION
 
-CENTRAL_ACC_ID=$(aws sts get-caller-identity --profile central --query Account)
-export CENTRAL_ACC_ID=${CENTRAL_ACC_ID//\"/}
-
 # Set profile for Customer Data Domain account - *customer*
 aws configure set profile.customer.aws_access_key_id $AWS_ACCESS_KEY_ID_CUSTOMER
 aws configure set profile.customer.aws_secret_access_key $AWS_SECRET_ACCESS_KEY_CUSTOMER
 aws configure set profile.customer.aws_session_token $AWS_SESSION_TOKEN_CUSTOMER
 aws configure set profile.customer.region $AWS_REGION
 
-CUSTOMER_ACC_ID=$(aws sts get-caller-identity --profile customer --query Account)
-export CUSTOMER_ACC_ID=${CUSTOMER_ACC_ID//\"/}
+# Validate profiles have been installed properly
+aws sts get-caller-identity --profile central > /dev/null 2>&1 || { echo &2 "[ERROR] aws profile 'central' is not properly configured. aborting..."; exit 1; }
+aws sts get-caller-identity --profile customer > /dev/null 2>&1 || { echo &2 "[ERROR] aws profile 'customer' is not properly configured. aborting..."; exit 1; }
